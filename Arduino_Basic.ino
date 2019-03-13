dc967c28345d782d15cab9a7f648f135e5d0cea2
dprt69 <43845241+dprt69@users.noreply.github.com>
2019-03-13 21:31:04 +0530

    Add files via upload

---
+++ Arduino_Basic.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+  digitalWrite(13,HIGH);
+  delay(850);
+  digitalWrite(13,LOW);
+  
+  // put your main code here, to run repeatedly:
+
+}
---
+++ sketch_feb14a.ino
@@ -0,0 +1,17 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  for(int i=0;i<5;i++);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  while(1);
+  
+  // put your main code here, to run repeatedly:
+
+}
